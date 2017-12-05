7449047a50be905e8afa6af1cab7f1221879a26b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-12-05 19:01:02 +0000

    run the right command

--- vars/cheribuildProject.groovy
+++ vars/cheribuildProject.groovy
@@ -167,7 +167,7 @@ def runCheribuild(CheribuildProjectParams proj) {
 			}
 			if (proj.needsFullCheriSDK) {
 				copyArtifacts projectName: "CHERI-SDK/ALLOC=jemalloc,ISA=vanilla,SDK_CPU=${proj.sdkCPU},label=${proj.label}", filter: '*-sdk.tar.xz', fingerprintArtifacts: true
-				sh "./cheribuild/cheribuild.py extract-sdk --cpu ${proj.cpu} ${proj.extraArgs}"
+				sh "./cheribuild/jenkins-cheri-build.py extract-sdk --cpu ${proj.cpu} ${proj.extraArgs}"
 			}
 			echo 'WORKSPACE after checkout:'
 			sh 'ls -la'
