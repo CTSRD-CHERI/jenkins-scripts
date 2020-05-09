63785e7c7d901358c92a896756a8495690f720e1
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-09 18:36:36 +0100

    Java string instead of groovy GString

--- vars/cheribuildProject.groovy
+++ vars/cheribuildProject.groovy
@@ -492,7 +492,7 @@ def runCheribuild(CheribuildProjectParams params) {
 	if (env.CHANGE_ID) {
 		def labels = pullRequest.labels
 		echo("PR Labels: ${labels}")
-		if (labels.any { it -> it == "NO-JENKINS"}) {
+		if (labels.any { it -> it == 'NO-JENKINS'}) {
 			echo("Skipping Jenkins for pull request since NO-JENKINS label was set")
 			return
 		}
