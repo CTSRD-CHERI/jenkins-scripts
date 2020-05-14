c4a669663c42d25074354d5b8836f4ff1e7c1802
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-14 23:00:25 +0100

    Fix access to property that doesn't exist yet

--- vars/cheribuildProject.groovy
+++ vars/cheribuildProject.groovy
@@ -497,10 +497,10 @@ def runCheribuild(CheribuildProjectParams params) {
 			return
 		}
 		// Depends on https://github.com/jenkinsci/pipeline-github-plugin/pull/77
-		if (pullRequest?.draft) {
-			echo("Skipping Jenkins for pull request since draft flag was set")
-			return
-		}
+		// if (pullRequest.draft) {
+		//	echo("Skipping Jenkins for pull request since draft flag was set")
+		//	return
+		//}
 		if (!pullRequest.mergeable) {
 			error("Pull request is not mergeable!")
 			return
