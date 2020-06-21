c6753f2a87561363ddb29120617e5574e8e038bc
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-06-21 21:40:18 +0100

    Fix stuff

--- vars/addJobProperties.groovy
+++ vars/addJobProperties.groovy
@@ -1,17 +1,28 @@
 import com.cloudbees.groovy.cps.NonCPS
 import hudson.model.JobProperty
 import groovy.json.JsonBuilder
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
 
+//def isMultiBranchPipeline() {
+//    return Jenkins.get().getItem(currentBuild.projectName) instanceof WorkflowMultiBranchProject
+//}
 
-// The properties() function **sets** job properties, deleting all previously set ones!
+// The properties() function **sets** job propertiesorg.jenkinsci.plugins.workflow.cps.RunWrapperBinder, deleting all previously set ones!
 // See e.g. https://issues.jenkins-ci.org/browse/JENKINS-44848
 // Hopefully this workaround works
 @NonCPS
 def call(List<JobProperty<?>> args) {
     hudson.model.Job<?, ?> job = currentBuild.rawBuild.parent;
-    echo("Properties before: ${new JsonBuilder(job.getAllProperties()).toPrettyString()}")
-    for (JobProperty<?> prop : args) {
-        job.removeProperty(prop);
+    echo("Properties before:")
+    job.getAllProperties().eachWithIndex{ JobProperty<?> entry, int i ->
+        echo("Property ${i}: ${entry.toString()}")
     }
-    echo("Properties after: ${new JsonBuilder(job.getAllProperties()).toPrettyString()}")
+    echo("Properties before:")
+    for (JobProperty<?> prop : args) {
+        job.addProperty(prop);
+    }
+    echo("Properties after:")
+    job.getAllProperties().eachWithIndex{ JobProperty<?> entry, int i ->
+        echo("Property ${i}: ${entry.toString()}")
+    }
 }
