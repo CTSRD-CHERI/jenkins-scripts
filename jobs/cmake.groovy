fcd464aa9e55763964ef325ba533f287bd9e3bbe
Marno van der Maas <34654485+marnovandermaas@users.noreply.github.com>
2021-10-19 15:56:04 +0100

    Fixed name of cmake RISCV64 for purecap rootFS

--- jobs/cmake.groovy
+++ jobs/cmake.groovy
@@ -8,7 +8,7 @@ setDefaultJobProperties([
 
 def cmakeRepo = gitRepoWithLocalReference(url: 'https://gitlab.kitware.com/cmake/cmake.git', branch: 'release')
 cheribuildProject(target: 'cmake',
-                  targetArchitectures: ["amd64", "riscv64-purecap", "riscv64"], // TODO: morello
+                  targetArchitectures: ["amd64", "riscv64-for-purecap-rootfs", "riscv64"], // TODO: morello
                   scmOverride: cmakeRepo,
                   skipArchiving: false,
                   runTests: false,
