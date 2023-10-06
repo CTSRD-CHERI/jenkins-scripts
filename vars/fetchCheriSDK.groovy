b36dbd19cb75b9b62830f54f2fb1df1dc1b6d846
Jessica Clarke <jrtc27@jrtc27.com>
2023-10-06 23:37:41 +0100

    fetchCheriSDK.groovy: Retire demo-2023-10

--- vars/fetchCheriSDK.groovy
+++ vars/fetchCheriSDK.groovy
@@ -47,7 +47,7 @@ def call(Map args) {
         gitBranch = env.BRANCH_NAME
     }
     if (!params.llvmBranch) {
-        if (gitBranch in ['c18n', 'caprevoke', 'cocall', 'cocalls', 'coexecve', 'dev', 'devel', 'demo-2023-10'])
+        if (gitBranch in ['c18n', 'caprevoke', 'cocall', 'cocalls', 'coexecve', 'dev', 'devel'])
             params.llvmBranch = 'dev'
         else if (gitBranch == 'abi-breaking-changes')
             params.llvmBranch = 'abi-breaking-changes'
