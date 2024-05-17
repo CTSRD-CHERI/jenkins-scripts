11976ed463594d7158714ca2572af1429fd32759
Jessica Clarke <jrtc27@jrtc27.com>
2024-05-17 19:09:26 +0100

    cheribsdInfo: New helper routines for things that use CheriBSD artifacts

---
+++ vars/cheribsdInfo.groovy
@@ -0,0 +1,27 @@
+def getDefaultKernelAbi(String branch = "main") {
+    if (branch == "releng/22.12" || branch == "releng/23.11" || branch == "main")
+        return "hybrid"
+    return "hybrid" // "purecap"
+}
+
+def getKernelConfig(String arch, String abi, String branch = "main") {
+    if (arch == "morello-hybrid" || arch == "morello-purecap") {
+        if (abi == "hybrid")
+            return "GENERIC-MORELLO"
+        if (abi == "purecap")
+            return "GENERIC-MORELLO-PURECAP"
+        if (abi == "purecap-benchmark")
+            return "GENERIC-MORELLO-PURECAP-BENCHMARK"
+        error("Unknown ${arch} kernel ABI: '${abi}'")
+        return null
+    }
+    if (arch == "riscv64-hybrid" || arch == "riscv64-purecap") {
+        if (abi == "hybrid")
+            return "CHERI-QEMU"
+        if (abi == "purecap")
+            return "CHERI-PURECAP-QEMU"
+        error("Unknown ${arch} kernel ABI: '${abi}'")
+        return null
+    }
+    error("Unknown architecture: '${arch}'")
+}
