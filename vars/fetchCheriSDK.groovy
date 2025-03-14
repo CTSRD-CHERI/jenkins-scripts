004c4bd67d592b5364928b16c084c4e4087cc8d2
Jessica Clarke <jrtc27@jrtc27.com>
2025-03-14 20:50:09 +0000

    fetchCheriSDK.groovy: Also use CHERI LLVM c18n_policy for dev_c18n

--- vars/fetchCheriSDK.groovy
+++ vars/fetchCheriSDK.groovy
@@ -55,6 +55,8 @@ def call(Map args) {
             params.llvmBranch = 'abi-breaking-changes'
         else if (gitBranch == 'upstream-llvm-merge')
             params.llvmBranch = 'upstream-llvm-merge'
+        else if (gitBranch == 'dev_c18n')
+            params.llvmBranch = 'c18n_policy'
         else
             params.llvmBranch = 'master'
         // echo("Inferred LLVM branch from current git branch (${gitBranch}): ${params.llvmBranch}")
