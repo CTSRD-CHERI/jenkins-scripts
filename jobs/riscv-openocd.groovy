0686276a7ba6ce5190e847581f1adf28ea4e911e
Franz Fuchs <francislaus@users.noreply.github.com>
2025-04-23 10:52:05 +0100

    Removed unnecessary patches in riscv-openocd.groovy

--- jobs/riscv-openocd.groovy
+++ jobs/riscv-openocd.groovy
@@ -22,19 +22,6 @@ index f2c467618..9422a9513 100644
  	uint32_t command = riscv013_access_register_command(target, number, size,
  			AC_ACCESS_REGISTER_TRANSFER);
 
-@@ -904,6 +905,12 @@ static int register_read_abstract_with_size(struct target *target,
- 				info->abstract_read_fpr_supported = false;
- 				LOG_TARGET_INFO(target, "Disabling abstract command reads from FPRs.");
- 			} else if (number >= GDB_REGNO_CSR0 && number <= GDB_REGNO_CSR4095) {
-+				/* XXX: Bluespec's DM doesn't support 32-bit access on RV64 */
-+				if (size == 32) {
-+					LOG_TARGET_INFO(target, "Retrying 64-bit read of CSR %x", number - GDB_REGNO_CSR0);
-+					size = 64;
-+					goto retry;
-+				}
- 				info->abstract_read_csr_supported = false;
- 				LOG_TARGET_INFO(target, "Disabling abstract command reads from CSRs.");
- 			}
 @@ -941,7 +948,8 @@ static int register_write_abstract(struct target *target, enum gdb_regno number,
  			!info->abstract_write_csr_supported)
  		return ERROR_FAIL;
@@ -45,19 +32,6 @@ index f2c467618..9422a9513 100644
  	const uint32_t command = riscv013_access_register_command(target, number, size_bits,
  			AC_ACCESS_REGISTER_TRANSFER |
  			AC_ACCESS_REGISTER_WRITE);
-@@ -971,6 +979,12 @@ static int register_write_abstract(struct target *target, enum gdb_regno number,
- 				info->abstract_write_fpr_supported = false;
- 				LOG_TARGET_INFO(target, "Disabling abstract command writes to FPRs.");
- 			} else if (number >= GDB_REGNO_CSR0 && number <= GDB_REGNO_CSR4095) {
-+				/* XXX: Bluespec's DM doesn't support 32-bit access on RV64 */
-+				if (size_bits == 32) {
-+					LOG_TARGET_INFO(target, "Retrying 64-bit write of CSR %x", number - GDB_REGNO_CSR0);
-+					size_bits = 64;
-+					goto retry;
-+				}
- 				info->abstract_write_csr_supported = false;
- 				LOG_TARGET_INFO(target, "Disabling abstract command writes to CSRs.");
- 			}
 @@ -1836,7 +1850,8 @@ static int reset_dm(struct target *target)
  				LOG_TARGET_ERROR(target, "DM didn't acknowledge reset in %d s. "
  						"Increase the timeout with 'riscv set_command_timeout_sec'.",
