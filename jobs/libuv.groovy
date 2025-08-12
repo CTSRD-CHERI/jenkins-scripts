2732e241e7f6b27826cec3dac630dcaf71b73c96
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-12 02:20:16 +0100

    jobs/libuv.groovy: New job, needed for CMake

---
+++ jobs/libuv.groovy
@@ -0,0 +1,15 @@
+@Library('ctsrd-jenkins-scripts') _
+
+// Set the default job properties (work around properties() not being additive but replacing)
+setDefaultJobProperties([
+        rateLimitBuilds([count: 2, durationName: 'hour', userBoost: true]),
+        copyArtifactPermission('*'),
+])
+
+// NB: Currently overrides https://github.com/libuv/libuv.git in cheribuild
+def libuvRepo = gitRepoWithLocalReference(url: 'https://github.com/arichardson/libuv.git')
+cheribuildProject(target: 'libuv',
+                  scmOverride: libuvRepo,
+                  targetArchitectures: ["aarch64", "amd64", "morello-purecap", "riscv64", "riscv64-purecap"],
+                  setGitHubStatus: false, // external repo
+)
