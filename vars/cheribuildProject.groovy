CTSRD-CHERI/jenkins-scripts

commit 93b46dba5d78a606a695c15f32760e1491d89357

vars/cheribuildProject.groovy: Unify parallel and single arch cases
This will make it easier to alter this code without having to duplicate
the changes.

vars/cheribuildProject.groovy
@@ -636,20 +636,30 @@ def call(Map args) {
 	List targetArchitectures = args.getOrDefault('targetArchitectures', [])
 	def failFast = args.getOrDefault('failFast', true) as Boolean
 	args.remove('failFast')
-	if (targetArchitectures.isEmpty()) {
-		return runCheribuild(parseParams(args))
+	boolean singleArch = targetArchitectures.isEmpty()
+	def taskArgs = [:]
+	if (singleArch) {
+		taskArgs[0] = args
+	} else {
+		targetArchitectures.each { String suffix ->
+			String targetWithoutSuffix = args.getOrDefault('target', 'target must be set!')
+			Map newMap = args + [target			  : targetWithoutSuffix + "-${suffix}",
+								 _targetWithoutSuffix: targetWithoutSuffix,
+								 architecture		: "${suffix}"]
+			echo("newMap=${newMap}")
+			taskArgs[suffix] = newMap
+		}
+	}
+	def tasks = [:]
+	taskArgs.each = { key, val ->
+		def params = parseParams(val)
+		tasks[key] = { -> return runCheribuild(params) }
+	}
+	if (singleArch) {
+		assert tasks.size() == 1
+		return tasks[0]()
 	}
 	// Otherwise run multiple architectures in parallel
-	def tasks = [:]
-	targetArchitectures.each { String suffix ->
-		String targetWithoutSuffix = args.getOrDefault('target', 'target must be set!')
-		Map newMap = args + [target			  : targetWithoutSuffix + "-${suffix}",
-							 _targetWithoutSuffix: targetWithoutSuffix,
-							 architecture		: "${suffix}"]
-		echo("newMap=${newMap}")
-		def params = parseParams(newMap)
-		tasks[suffix] = { -> return runCheribuild(params) }
-	}
 	if (env?.UNIT_TEST) {
 		tasks.each { key, closure ->
 			echo("Running ${key}")

commit 9d5172bd6126b4808115200d4d50379fe5ad78ca

vars/cheribuildProject.groovy: Fix typo
Fixes: 93b46dba5d78 ("vars/cheribuildProject.groovy: Unify parallel and single arch cases")

vars/cheribuildProject.groovy
@@ -650,7 +650,7 @@ def call(Map args) {
 	def tasks = [:]
 	boolean shouldBuild = false
 	def skippedBuildTask = { -> echo "Not building this pull request." }
-	taskArgs.each = { key, val ->
+	taskArgs.each { key, val ->
 		def params = parseParams(val)
 		if (env.CHANGE_ID && !shouldBuildPullRequest(context: params.gitHubStatusContext)) {
 			tasks[key] = skippedBuildTask