CTSRD-CHERI/jenkins-scripts

commit 2732e241e7f6b27826cec3dac630dcaf71b73c96

jobs/libuv.groovy: New job, needed for CMake

jobs/libuv.groovy
@@ -1,0 +1,15 @@
+@Library('ctsrd-jenkins-scripts') _
+
+// Set the default job properties (work around properties() not being additive but replacing)
+setDefaultJobProperties([
+        rateLimitBuilds([count: 2, durationName: 'hour', userBoost: true]),
+        copyArtifactPermission('*'),
+])
+
+// NB: Currently overrides https://github.com/libuv/libuv.git in cheribuild
+def libuvRepo = gitRepoWithLocalReference(url: 'https://github.com/arichardson/libuv.git')
+cheribuildProject(target: 'libuv',
+                  scmOverride: libuvRepo,
+                  targetArchitectures: ["aarch64", "amd64", "morello-purecap", "riscv64", "riscv64-purecap"],
+                  setGitHubStatus: false, // external repo
+)

commit ab7372d8bb64c86834274a79ad3b3bafa7af4a6d

jobs/libuv.groovy: Specify branch to build as it's not master
Fixes: 2732e241e7f6 ("jobs/libuv.groovy: New job, needed for CMake")

jobs/libuv.groovy
@@ -7,7 +7,7 @@ setDefaultJobProperties([
 ])
 
 // NB: Currently overrides https://github.com/libuv/libuv.git in cheribuild
-def libuvRepo = gitRepoWithLocalReference(url: 'https://github.com/arichardson/libuv.git')
+def libuvRepo = gitRepoWithLocalReference(url: 'https://github.com/arichardson/libuv.git', branch: 'v1.x')
 cheribuildProject(target: 'libuv',
                   scmOverride: libuvRepo,
                   targetArchitectures: ["aarch64", "amd64", "morello-purecap", "riscv64", "riscv64-purecap"],