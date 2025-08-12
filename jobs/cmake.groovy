07ca386a16c6e8581ac1a0c67f0fa2f6473a6f46
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-12 02:31:32 +0100

    jobs/cmake.groovy: Add dependency on libuv

--- jobs/cmake.groovy
+++ jobs/cmake.groovy
@@ -7,9 +7,13 @@ setDefaultJobProperties([
 ])
 
 def cmakeRepo = gitRepoWithLocalReference(url: 'https://gitlab.kitware.com/cmake/cmake.git', branch: 'release')
+def sysrootDeps = [
+        [target: "libuv", job: "libuv", filter: { params -> params.architecture != "native" }],
+]
 cheribuildProject(target: 'cmake',
                   targetArchitectures: ["native", "aarch64", "amd64", "morello-purecap", "riscv64", "riscv64-purecap"],
                   scmOverride: cmakeRepo,
+                  sysrootDependencies: sysrootDeps,
                   skipArchiving: false,
                   runTests: false,
                   setGitHubStatus: false, // external repo
