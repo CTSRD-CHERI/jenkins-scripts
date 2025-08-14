CTSRD-CHERI/jenkins-scripts

commit 7d83cdb767c97cfdf9a1ca6222cf4e691e507ed5

vars/shellQuote.groovy: New function
Fixes: 98ba5a94cb37 ("vars/cheribuildProject.groovy: Allow passing List for extraArgs")

vars/shellQuote.groovy
@@ -1,0 +1,7 @@
+String call(String s) {
+    // Safe character set according to Python 3.13
+    // NB: Empty string needs quoting too
+    if (s ==~ /[\w@%+=:,./-]+/)
+        return s
+    return "'" + s.replaceAll('\'', '\\\'') + "'"
+}

commit 85083f1934e0e1aa32265094f8829f89417b928b

vars/shellQuote.groovy: Properly escape \ in replacement
Java's replaceAll takes a pattern as the replacement, where $ is special
and \ is used for escaping, so we need to add another (escaped) layer of
escaping otherwise we end up replacing ' with '.

Fixes: 7d83cdb767c9 ("vars/shellQuote.groovy: New function")

vars/shellQuote.groovy
@@ -3,5 +3,5 @@ String call(String s) {
     // NB: Empty string needs quoting too
     if (s ==~ /[\w@%+=:,.\/-]+/)
         return s
-    return "'" + s.replaceAll('\'', '\\\'') + "'"
+    return "'" + s.replaceAll('\'', '\\\\\'') + "'"
 }