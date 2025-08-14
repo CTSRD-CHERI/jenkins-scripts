CTSRD-CHERI/jenkins-scripts

commit 7d83cdb767c97cfdf9a1ca6222cf4e691e507ed5

vars/shellQuote.groovy: New function
Fixes: 98ba5a94cb37 ("vars/cheribuildProject.groovy: Allow passing List for extraArgs")

vars/shellQuote.groovy
@@ -1,0 +1,7 @@
+String call(String s) {
+    // Safe character set according to Python 3.13
+    // NB: Empty string needs quoting too
+    if (s ==~ /[\w@%+=:,./-]+/)
+        return s
+    return "'" + s.replaceAll('\'', '\\\'') + "'"
+}

commit 206fd9671491dea2d5b0b95b8e16e57b05a991de

vars/shellQuote.groovy: Add missing escape in regex
Fixes: 7d83cdb767c9 ("vars/shellQuote.groovy: New function")

vars/shellQuote.groovy
@@ -1,7 +1,7 @@
 String call(String s) {
     // Safe character set according to Python 3.13
     // NB: Empty string needs quoting too
-    if (s ==~ /[\w@%+=:,./-]+/)
+    if (s ==~ /[\w@%+=:,.\/-]+/)
         return s
     return "'" + s.replaceAll('\'', '\\\'') + "'"
 }