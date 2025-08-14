7d83cdb767c97cfdf9a1ca6222cf4e691e507ed5
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-14 21:50:32 +0100

    vars/shellQuote.groovy: New function
    Fixes: 98ba5a94cb37 ("vars/cheribuildProject.groovy: Allow passing List for extraArgs")

---
+++ vars/shellQuote.groovy
@@ -0,0 +1,7 @@
+String call(String s) {
+    // Safe character set according to Python 3.13
+    // NB: Empty string needs quoting too
+    if (s ==~ /[\w@%+=:,./-]+/)
+        return s
+    return "'" + s.replaceAll('\'', '\\\'') + "'"
+}
