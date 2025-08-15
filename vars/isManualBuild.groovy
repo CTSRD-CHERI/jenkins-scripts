82b440955e90b7afad9b2294360aac8707c66c57
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-15 21:06:38 +0100

    vars/isManualBuild.groovy: Treat GitHub comment triggers as manual

--- vars/isManualBuild.groovy
+++ vars/isManualBuild.groovy
@@ -2,7 +2,8 @@ def call() {
     def manualBuild = false
     currentBuild.buildCauses.eachWithIndex { item, index ->
         echo "Build cause ${index}: ${item} -- class=${item._class}"
-        if ("${item._class}" == 'hudson.model.Cause$UserIdCause')
+        if ("${item._class}" == 'hudson.model.Cause$UserIdCause' ||
+            "${item._class}" == 'org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause')
             manualBuild = true
     }
     return manualBuild
