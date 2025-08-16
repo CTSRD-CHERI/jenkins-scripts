CTSRD-CHERI/jenkins-scripts

commit 072dfadcc7e448652393bc8957d71f0c569f9f1f

vars/cheribuildProject.groovy: Attempt to fix long-standing status race
PullRequestGlobalVariable appears to not save the new object in the
script's bindings like other subclasses of GlobalVariable, and so I am
assuming that the racing we see is because every reference gets a fresh
instance, and it is during the constructor that the PR's metadata,
including the head commit, is retrieved. Even if it did, there would
still technically be a slight race between the build being triggered for
a specific commit and the first time pullRequest is referenced, so
although that's a bug in the plugin, we should still handle the case
where the pullRequest object does not correspond to what we're building.

This is all rather grotty due to GIT_COMMIT being the result of merging
the PR into the target branch (which may be a fast-forward, so we can't
always just take the second parent), with no equivalent for the
underlying PR commit we're testing.

vars/cheribuildProject.groovy
@@ -155,7 +155,17 @@ boolean updatePRStatus(CheribuildProjectParams proj, String message, String stat
 	if (!proj.setGitHubStatus) {
 		return false
 	}
+
+	def expectedCommit = proj.getRepoInfoForGitHubStatus()?.GIT_COMMIT
 	try {
+		// Not sure if the current GlobalVariable for this will create a fresh copy on every reference within
+		// the same function or not, so use a local variable to be sure.
+		def pr = pullRequest
+		// If the PR's head has changed, fall back on updating the commit
+		if (expectedCommit != null && pr.head != expectedCommit) {
+			echo("PR${env.CHANGE_ID} head ${pr.head} not ${expectedCommit}")
+			return false
+		}
 		if (!status) {
 			status = proj._result.name().toLowerCase()
 			if (status == 'failure') {
@@ -165,7 +175,7 @@ boolean updatePRStatus(CheribuildProjectParams proj, String message, String stat
 			}
 		}
 		echo("Setting PR${env.CHANGE_ID} status: ${status} for ${proj.gitHubStatusContext}: ${message}")
-		pullRequest.createStatus(status: status,
+		pr.createStatus(status: status,
 				context: proj.gitHubStatusContext,
 				description: message,
 				targetUrl: "${env.RUN_DISPLAY_URL}")
@@ -361,6 +371,30 @@ def runCheribuildImpl(CheribuildProjectParams proj) {
 	}
 }
 
+@NonCPS
+String getMarkedCommit(String headSha) {
+	def rawBuild = currentBuild.rawBuild
+	if (rawBuild == null) {
+		return null
+	}
+	def actions = rawBuild.getActions(hudson.plugins.git.util.BuildData)
+	def marked = [] as Set
+	actions.each {
+		def lastBuild = it.lastBuild
+		if (lastBuild != null && lastBuild.revision == headSha) {
+			marked += lastBuild.marked.sha1String
+		}
+	}
+	if (marked.empty) {
+		return null
+	}
+	if (marked.size() > 1) {
+		echo("Found multiple possible marked commits: ${marked}")
+		return null
+	}
+	return marked[0]
+}
+
 def runCheribuildImplWithEnv(CheribuildProjectParams proj) {
 	def gitHubCommitSHA = null
 	def gitHubRepoURL = null
@@ -395,6 +429,15 @@ def runCheribuildImplWithEnv(CheribuildProjectParams proj) {
 				gitHubRepoURL = proj.gitInfo?.GIT_URL
 				if (proj.gitInfoMap != null) {
 					proj.gitInfoMap << proj.gitInfo
+				}
+				// For PRs we want the status to be attached to the branch's commit, not the test merge
+				// commit that we've checked out.
+				if (env.CHANGE_ID && gitHubCommitSHA != null && proj.gitHubStatusArgs == null) {
+					def markedCommit = getMarkedCommit(gitHubCommitSHA)
+					if (markedCommit != null) {
+						echo("Using ${markedCommit} for PR status updates")
+						proj.gitHubStatusArgs = proj.gitInfo + [GIT_COMMIT: markedCommit]
+					}
 				}
 				// store it so it exists even on exception
 			}

commit 21ccd081369b6a0d88ed0b17e6ed6ec526bf95e8

vars/cheribuildProject.groovy: Compare against hash not revision object
Fixes: 072dfadcc7e4 ("vars/cheribuildProject.groovy: Attempt to fix long-standing status race")

vars/cheribuildProject.groovy
@@ -381,7 +381,7 @@ String getMarkedCommit(String headSha) {
 	def marked = [] as Set
 	actions.each {
 		def lastBuild = it.lastBuild
-		if (lastBuild != null && lastBuild.revision == headSha) {
+		if (lastBuild != null && lastBuild.revision.sha1String == headSha) {
 			marked += lastBuild.marked.sha1String
 		}
 	}