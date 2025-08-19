CTSRD-CHERI/jenkins-scripts

commit c0cc19376858e13d8f1a2f19f5f55cc1ab0aa4bf

vars/setGitHubStatus.groovy: Treat result as part of default message
Also pull the duration up. This puts the summary first and then the test
status optionally afterwards.

vars/setGitHubStatus.groovy
@@ -25,15 +25,17 @@ def call(Map<String, String> args = [:]) {
             String description = currentBuild.displayName
             if (description == null)
                 description = ''
-            message = "${prettyJobName}: ${description}"
+            message = "${prettyJobName}: ${description} ${result}"
         }
-        if (includeTestStatus)
-            message += "\n${getTestStatus()}"
 
         if (currentBuild.durationString && result != 'PENDING') {
             String duration = currentBuild.durationString.replaceAll(/and counting$/, "")
-            message += "\n${result} after ${duration}"
+            message += " in ${duration}"
         }
+
+        if (includeTestStatus)
+            message += "\n${getTestStatus()
+
         // Map from Jenkins states to GitHub states: https://github.com/github-api/github-api/blob/master/src/main/java/org/kohsuke/github/GHCommitState.java
         if (result == 'UNSTABLE')
             result = 'FAILURE';

commit d4fdf87d0b6b79ccc069e34fa6613b664738c84e

vars/setGitHubStatus.groovy: Add missing }"
Fixes: c0cc19376858 ("vars/setGitHubStatus.groovy: Treat result as part of default message")

vars/setGitHubStatus.groovy
@@ -34,7 +34,7 @@ def call(Map<String, String> args = [:]) {
         }
 
         if (includeTestStatus)
-            message += "\n${getTestStatus()
+            message += "\n${getTestStatus()}"
 
         // Map from Jenkins states to GitHub states: https://github.com/github-api/github-api/blob/master/src/main/java/org/kohsuke/github/GHCommitState.java
         if (result == 'UNSTABLE')