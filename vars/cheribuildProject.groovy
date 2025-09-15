CTSRD-CHERI/jenkins-scripts

commit 98ba5a94cb37cab7a43665dea075cbd36d4df9c7

vars/cheribuildProject.groovy: Allow passing List for extraArgs
Rather than require the caller to provide a properly-quoted string,
allow the caller to pass the list of unquoted arguments, as a more
friendly API.

vars/cheribuildProject.groovy
@@ -67,7 +67,8 @@ class CheribuildProjectParams implements Serializable {
 	/// general/build parameters
 	String target // the cheribuild project name
 	String _targetWithoutSuffix  // Set when building multiple architectures
-	String extraArgs = '' // additional arguments to pass to cheribuild.py
+	def extraArgs = '' // additional arguments to pass to cheribuild.py
+	// A String (quoted) or List of String (unquoted), normalised to a String (quoted) by parseParams
 	String cpu = 'default' // --cpu flag for cheribuild (deprecated)
 	String architecture // suffix to be used for all output files, etc.
 	String sysrootArchitecture // sysroot architecture (e.g. riscv64-purecap for foo-hybrid-for-purecap-rootfs)
@@ -598,6 +599,13 @@ CheribuildProjectParams parseParams(Map args) {
 	}
 	if (!params.gitHubStatusContext) {
 		params.gitHubStatusContext = "jenkins/${params.uniqueId}"
+	}
+
+	if (params.extraArgs instanceof List) {
+		params.extraArgs = params.extraArgs.collect({ shellQuote(it) }).join(" ")
+	}
+	if (!(params.extraArgs instanceof String)) {
+		throw new IllegalArgumentException("extraArgs must be a String or List of String")
 	}
 
 	// WTF. Work around weird scoping rules. Groovy really sucks...

commit 2acf13460bda5c2a9bf475371db4ad840ce337f0

vars/cheribuildProject.groovy: Handle GString for extraArgs
This is the internal type used for lazy-evaluated interpolated strings.
Normally it's implicitly converted when assigned to a variable of type
String but we need to explicitly handle it here.

Fixes: 98ba5a94cb37 ("vars/cheribuildProject.groovy: Allow passing List for extraArgs")

vars/cheribuildProject.groovy
@@ -675,6 +675,9 @@ CheribuildProjectParams parseParams(Map args) {
 	if (params.extraArgs instanceof List) {
 		params.extraArgs = params.extraArgs.collect({ shellQuote(it) }).join(" ")
 	}
+	if (params.extraArgs instanceof GString) {
+		params.extraArgs = params.extraArgs.toString()
+	}
 	if (!(params.extraArgs instanceof String)) {
 		throw new IllegalArgumentException("extraArgs must be a String or List of String")
 	}