5ad7779e7c18c57be9930d4d10a600fcb4745d57
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-09-14 21:56:45 -0700

    Sync QEMU job test with latest GitHub

--- test-scripts/qemu.groovy
+++ test-scripts/qemu.groovy
@@ -6,9 +6,9 @@ class GlobalVars { // "Groovy"
 }
 
 def jobProperties = [
-    rateLimitBuilds(throttle: [count: 2, durationName: 'hour', userBoost: true]),
-    [$class: 'GithubProjectProperty', projectUrlStr: 'https://github.com/CTSRD-CHERI/qemu'],
-    copyArtifactPermission('*'), // Downstream jobs need QEMU
+        rateLimitBuilds(throttle: [count: 2, durationName: 'hour', userBoost: true]),
+        [$class: 'GithubProjectProperty', projectUrlStr: 'https://github.com/CTSRD-CHERI/qemu'],
+        copyArtifactPermission('*'), // Downstream jobs need QEMU
 ]
 
 // Don't archive binaries for pull requests and non-default branches:
@@ -25,8 +25,8 @@ def paramsArray = []
 // Add an OS selector for manual builds
 def allConfigs = ["linux", "freebsd", "linux-debug"]
 paramsArray.add(text(defaultValue: allConfigs.join('\n'),
-        description: 'The configurations to build for (one per line)',
-        name: 'Configs'))
+                     description: 'The configurations to build for (one per line)',
+                     name: 'Configs'))
 
 jobProperties.add(parameters(paramsArray))
 
@@ -46,12 +46,103 @@ def maybeArchiveArtifacts(params, String os) {
         stage("Archiving artifacts") {
             // Add all the firmwares that are needed to boot CheriBSD
             def firmwareFiles = [
-                "efi-pcnet.rom", "vgabios-cirrus.bin", // MIPS
-                "bbl-riscv64cheri-virt-fw_jump.bin", // RISC-V
-                "bios-256k.bin", "efi-virtio.rom", "vgabios-stdvga.bin", // x86_64
-                "edk2-aarch64-code.fd"  // AArch64
+                    "efi-pcnet.rom", "vgabios-cirrus.bin", // MIPS
+                    "bbl-riscv64cheri-virt-fw_jump.bin", // RISC-V
+                    "bios-256k.bin", "efi-virtio.rom", "vgabios-stdvga.bin", // x86_64
+                    "edk2-aarch64-code.fd"  // AArch64
             ].collect { "qemu-${os}/share/qemu/$it" }.join(', ')
             archiveArtifacts allowEmptyArchive: false, artifacts: "qemu-${os}/bin/qemu-system-*, qemu-${os}/bin/qemu-img, ${firmwareFiles}", fingerprint: true, onlyIfSuccessful: true
+        }
+    }
+}
+
+def addBootJobs(bootJobs, params, String qemuConfig, String architecture, String cheribsdBranch, String suffix="") {
+    // For purecap architectures we boot both hybrid and purecap kernels.
+    if (architecture.endsWith("-purecap")) {
+        bootJobs["${architecture}-purecap-kernel${suffix} on ${qemuConfig}"] =
+                { -> bootCheriBSD(params, qemuConfig, "${architecture}-purecap-kernel${suffix}", architecture,
+                                  'purecap', cheribsdBranch) }
+        bootJobs["${architecture}-hybrid-kernel${suffix} on ${qemuConfig}"] =
+                { -> bootCheriBSD(params, qemuConfig, "${architecture}-hybrid-kernel${suffix}", architecture,
+                                  'hybrid', cheribsdBranch) }
+    } else {
+        bootJobs["${architecture} on ${qemuConfig}"] =
+                { -> bootCheriBSD(params, qemuConfig, "${architecture}${suffix}", architecture, null, cheribsdBranch) }
+    }
+}
+
+def bootCheriBSDForAllArchitectures(params, String qemuConfig, boolean isDebug) {
+    stage("Boot CheriBSD (${qemuConfig})") {
+        def bootJobs = [failFast: false]
+        ["riscv64", "riscv64-purecap", "aarch64", "morello-purecap"].each { String architecture ->
+            addBootJobs(bootJobs, params, qemuConfig, architecture, "main")
+            if (!isDebug) {
+                // For the non-ASAN build of QEMU we also boot the latest release
+                def latestRelease = cheribsdInfo.getReleasedVersions()[-1]
+                addBootJobs(bootJobs, params, qemuConfig, architecture, "releng/${latestRelease}", "-latest-release")
+            }
+            def targetBranch = env.CHANGE_TARGET ? env.CHANGE_TARGET : env.BRANCH_NAME;
+            if (targetBranch == 'dev') {
+                addBootJobs(bootJobs, params, qemuConfig, architecture, "dev", "-dev")
+            }
+        }
+        parallel bootJobs
+    }
+}
+
+def bootCheriBSD(params, String qemuConfig, String stageSuffix, String archSuffix, String kernelABI, String cheribsdBranch="main") {
+    try {
+        def defaultKernelAbi = cheribsdInfo.getDefaultKernelAbi(cheribsdBranch)
+        def extraCheribuildArgs = [
+                "--cheribsd/default-kernel-abi=${defaultKernelAbi}"
+        ]
+        def compressedKernel = "artifacts-${archSuffix}/kernel.xz"
+        if (kernelABI != null) {
+            extraCheribuildArgs += ["--run-${archSuffix}/kernel-abi=${kernelABI}"]
+            if (archSuffix.startsWith('riscv64') && kernelABI != defaultKernelAbi) {
+                def kernelConfig = cheribsdInfo.getKernelConfig(archSuffix, kernelABI, cheribsdBranch)
+                compressedKernel = "artifacts-${archSuffix}/kernel.${kernelConfig}.xz"
+            }
+        }
+        def compressedDiskImage = "artifacts-${archSuffix}/cheribsd-${archSuffix}.img.xz"
+        dir (stageSuffix) {
+            sh "rm -rfv artifacts-${archSuffix}/cheribsd-*.img* artifacts-${archSuffix}/kernel*"
+            def jenkinsBranchName = cheribsdBranch.replaceAll('/', '%2F')
+            copyArtifacts projectName: "CheriBSD-pipeline/${jenkinsBranchName}", filter: "${compressedDiskImage}, ${compressedKernel}",
+                          target: '.', fingerprintArtifacts: false, flatten: false, selector: lastSuccessful()
+        }
+        def testExtraArgs = [
+                '--no-timestamped-test-subdir',
+                '--no-keep-compressed-images',
+                // Run a small subset of kyua tests to match the CheriBSD CI
+                '--kyua-tests-files=/usr/tests/bin/cat/Kyuafile',
+                "--test-output-dir=\$WORKSPACE/test-results/${stageSuffix}",
+                "--disk-image=${stageSuffix}/${compressedDiskImage}",
+                "--kernel=${stageSuffix}/${compressedKernel}",
+        ]
+        sh label: 'generate SSH key',
+           script: 'test -e $WORKSPACE/id_ed25519 || ssh-keygen -t ed25519 -N \'\' -f $WORKSPACE/id_ed25519 < /dev/null'
+        sh label: "Boot CheriBSD (${stageSuffix})", script: """
+    rm -rf test-results/${stageSuffix} && mkdir -p test-results/${stageSuffix}
+    ./cheribuild/jenkins-cheri-build.py --test run-${archSuffix} '--test-extra-args=${testExtraArgs.join(" ")}' \
+        --test-ssh-key \$WORKSPACE/id_ed25519.pub ${extraCheribuildArgs.join(" ")} || echo Boot test failed
+    """
+        def summary = junitReturnCurrentSummary allowEmptyResults: false, keepLongStdio: true,
+                                                testResults: "test-results/${stageSuffix}/test-results.xml"
+        def testResultMessage = "Test summary: ${summary.totalCount}, Failures: ${summary.failCount}, Skipped: ${summary.skipCount}, Passed: ${summary.passCount}"
+        echo("${stageSuffix}: ${testResultMessage}")
+        if (summary.passCount == 0 || summary.totalCount == 0) {
+            params.statusFailure("No tests successful?")
+        } else if (summary.failCount != 0) {
+            // Note: Junit set should have set stage/build status to unstable already, but we still need to set
+            // the per-configuration status, since Jenkins doesn't have a build result for each parallel branch.
+            params.statusUnstable("Unstable test results: ${testResultMessage}")
+            // If there were test failures, we archive the JUnitXML file to simplify debugging
+            archiveArtifacts allowEmptyArchive: true, artifacts: "test-results/${stageSuffix}/*.xml", onlyIfSuccessful: false
+        }
+    } finally {
+        dir (stageSuffix) {
+            deleteDir()
         }
     }
 }
@@ -73,21 +164,22 @@ selectedConfigs.each { config ->
             def extraQemuArgs = ''
             boolean isDebug = config.endsWith('-debug')
             if (isDebug) {
-                extraQemuArgs = '--qemu/configure-options=--enable-rvfi-dii --qemu/build-type=Debug --qemu/use-asan'
+                extraQemuArgs = '"--qemu/configure-options=--enable-rvfi-dii --extra-cflags=-O2 --extra-cxxflags=-O2" --qemu/build-type=Debug'
             }
             def qemuResult = cheribuildProject(target: 'qemu', cpu: 'native', skipArtifacts: true,
-                    nodeLabel: null,
-                    extraArgs: "--without-sdk --install-prefix=/usr $extraQemuArgs",
-                    runTests: /* true */ false,
-                    uniqueId: "qemu-build-${config}",
-                    skipTarball: true,
-                    afterBuild: { params ->
-                        extraBuildSteps(params, os)
-                        // Don't archive the Debug+ASAN artifacts
-                        if (!isDebug) {
-                            maybeArchiveArtifacts(params, os)
-                        }
-                    })
+                                               nodeLabel: null,
+                                               extraArgs: "--without-sdk --install-prefix=/usr $extraQemuArgs",
+                                               runTests: true,
+                                               uniqueId: "qemu-build-${config}",
+                                               skipTarball: true,
+                                               afterBuild: { params ->
+                                                   extraBuildSteps(params, os)
+                                                   bootCheriBSDForAllArchitectures(params, config, isDebug)
+                                                   // For Debug+ASAN we don't archive artifacts.
+                                                   if (!isDebug) {
+                                                       maybeArchiveArtifacts(params, os)
+                                                   }
+                                               })
 
             // Run the generated Morello tests
             stage("Run Morello tests") {
@@ -112,16 +204,16 @@ pytest qemu/tests/morello \
             // Run the baremetal MIPS tests to check we didn't regress.
             if (os == 'linux') {
                 cheribuildProject(target: 'cheritest-qemu', architecture: 'native',
-                        customGitCheckoutDir: 'cheritest', scmOverride: gitRepoWithLocalReference(url: 'https://github.com/CTSRD-CHERI/cheritest.git'),
-                        nodeLabel: null,
-                        // Ensure that test failures don't prevent creation of the junit file
-                        extraArgs: '--install-prefix=/ --cheritest-qemu/no-run-tests-with-build',
-                        runTests: true,
-                        // Set the status message on the QEMU repo not the cheritest one
-                        gitHubStatusArgs: qemuResult.gitInfo,
-                        sdkCompilerOnly: true, skipTarball: true,
-                        uniqueId: "mips-baremetal-testsuite-${config}",
-                        junitXmlFiles: 'cheritest/nosetests_qemu*.xml')
+                                  customGitCheckoutDir: 'cheritest', scmOverride: gitRepoWithLocalReference(url: 'https://github.com/CTSRD-CHERI/cheritest.git'),
+                                  nodeLabel: null,
+                                  // Ensure that test failures don't prevent creation of the junit file
+                                  extraArgs: '--install-prefix=/ --cheritest-qemu/no-run-tests-with-build',
+                                  runTests: true,
+                                  // Set the status message on the QEMU repo not the cheritest one
+                                  gitHubStatusArgs: qemuResult.gitInfo,
+                                  sdkCompilerOnly: true, skipTarball: true,
+                                  uniqueId: "mips-baremetal-testsuite-${config}",
+                                  junitXmlFiles: 'cheritest/nosetests_qemu*.xml')
             }
         }
     }
